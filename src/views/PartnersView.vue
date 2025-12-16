d5536a4042736606237d0da3e1083c4bee4e0c26
deco-1 <andrerfpsantos@proton.me>
2025-12-16 13:22:21 +0000

    partners and sponsors spacement

--- src/views/PartnersView.vue
+++ src/views/PartnersView.vue
@@ -1,18 +1,18 @@
 <script setup>
 import { computed, onMounted } from 'vue'
 import { useRoute } from 'vue-router'
-import { storeToRefs } from 'pinia';
-import { usePartnersStore } from '@/stores/partners';
-import '../assets/Partners&Sponsors.css';
+import { storeToRefs } from 'pinia'
+import { usePartnersStore } from '@/stores/partners'
+import '../assets/Partners&Sponsors.css'
 
-const route = useRoute();
-const accColor = computed(() => route.meta.accColor);
+const route = useRoute()
+const accColor = computed(() => route.meta.accColor)
 
-const partnersStore = usePartnersStore();
-const { partners } = storeToRefs(partnersStore);
+const partnersStore = usePartnersStore()
+const { partners } = storeToRefs(partnersStore)
 onMounted(async () => {
-  await partnersStore.fetchData();
-});
+  await partnersStore.fetchData()
+})
 </script>
 
 <template>
@@ -21,18 +21,27 @@ onMounted(async () => {
 
     <section class="intro">
       <h1 :style="`--acc-color: var(${accColor});`">Partners</h1>
-      <p>Get to know our amazing partners present during the week, be it with a stand at the job fair or with a
-        soft-skills workshop!</p>
+      <p>
+        Get to know our amazing partners present during the week, be it with a stand at the job fair
+        or with a soft-skills workshop!
+      </p>
     </section>
 
     <template v-for="tier in partners" :key="tier.tier">
       <template v-if="tier.companies.length > 0">
-        <div class="highlight-divider" :style="`--acc-color: var(${accColor});`"></div>
-        <section class="tier" :class="tier.tier">
-          <h2><span class="highlight-title">{{ tier.title.highlight }}</span>{{ " " + tier.title.normal }}</h2>
+        <!-- <div class="highlight-divider" :style="`--acc-color: var(${accColor});`"></div> -->
+        <section class="tier tier_spacer" :class="tier.tier">
+          <h2>
+            <span class="highlight-title">{{ tier.title.highlight }}</span
+            >{{ ' ' + tier.title.normal }}
+          </h2>
           <div class="container">
             <div v-for="(company, index) in tier.companies" :key="index" class="item">
-              <img v-if="company.image_base64 != null" :src="company.image_base64" :alt="company.name" />
+              <img
+                v-if="company.image_base64 != null"
+                :src="company.image_base64"
+                :alt="company.name"
+              />
               <p v-else>{{ company.name }}</p>
             </div>
           </div>
--- src/views/SponsorsView.vue
+++ src/views/SponsorsView.vue
@@ -1,18 +1,18 @@
 <script setup>
 import { computed, onMounted } from 'vue'
 import { useRoute } from 'vue-router'
-import { storeToRefs } from 'pinia';
-import { useSponsorsStore } from '@/stores/sponsors';
-import '../assets/Partners&Sponsors.css';
+import { storeToRefs } from 'pinia'
+import { useSponsorsStore } from '@/stores/sponsors'
+import '../assets/Partners&Sponsors.css'
 
-const route = useRoute();
-const accColor = computed(() => route.meta.accColor);
+const route = useRoute()
+const accColor = computed(() => route.meta.accColor)
 
-const sponsorsStore = useSponsorsStore();
-const { sponsors } = storeToRefs(sponsorsStore);
+const sponsorsStore = useSponsorsStore()
+const { sponsors } = storeToRefs(sponsorsStore)
 onMounted(async () => {
-  await sponsorsStore.fetchData();
-});
+  await sponsorsStore.fetchData()
+})
 </script>
 
 <template>
@@ -21,15 +21,20 @@ onMounted(async () => {
 
     <section class="intro">
       <h1 :style="`--acc-color: var(${accColor});`">Sponsors</h1>
-      <p>Our sponsors help us improve JEEC and without them the whole experience wouldn’t be as good as it is. Don’t
-        forget to look for merchandising and gifts!</p>
+      <p>
+        Our sponsors help us improve JEEC and without them the whole experience wouldn’t be as good
+        as it is. Don’t forget to look for merchandising and gifts!
+      </p>
     </section>
 
     <template v-for="tier in sponsors" :key="tier.tier">
       <template v-if="tier.companies.length > 0">
-        <div class="highlight-divider" :style="`--acc-color: var(${accColor});`"></div>
-        <section class="tier" :class="tier.tier">
-          <h2><span class="highlight-title">{{ tier.title.highlight }}</span>{{ " " + tier.title.normal }}</h2>
+        <!-- <div class="highlight-divider" :style="`--acc-color: var(${accColor});`"></div> -->
+        <section class="tier tier_spacer" :class="tier.tier">
+          <h2>
+            <span class="highlight-title">{{ tier.title.highlight }}</span
+            >{{ ' ' + tier.title.normal }}
+          </h2>
           <div class="container">
             <div v-for="(company, index) in tier.companies" :key="index" class="item">
               <img v-if="company.image != null" :src="company.image" :alt="company.name" />
